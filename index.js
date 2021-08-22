cdfd2442954f32369603a4eae1342ed952ab60e0
Kenan Yildirim <kenan@kenany.me>
2021-08-22 14:28:14 -0400

    chore(deps): @kenan/eslint-config@9.0.4
    Closes https://github.com/KenanY/mayhew/pull/65

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * @param {number} w Weight,
+ * @param {number} r Reps.
+ * @returns {number}
+ */
 function mayhew(w, r) {
   return 100 * w / (52.2 + 41.9 * Math.exp(-0.055 * r));
 }
--- test/index.js
+++ test/index.js
@@ -1,20 +1,20 @@
-var almostEqual = require('almost-equal');
-var isFunction = require('lodash/isFunction');
-var test = require('tape');
+const almostEqual = require('almost-equal');
+const isFunction = require('lodash/isFunction');
+const test = require('tape');
 
-var mayhew = require('../');
+const mayhew = require('../');
 
-test('exports a function', function(t) {
+test('exports a function', (t) => {
   t.plan(1);
   t.ok(isFunction(mayhew));
 });
 
-test('100 for 10', function(t) {
+test('100 for 10', (t) => {
   t.plan(1);
   t.ok(almostEqual(mayhew(100, 10), 130, 1));
 });
 
-test('100 for 6', function(t) {
+test('100 for 6', (t) => {
   t.plan(1);
   t.ok(almostEqual(mayhew(100, 6), 121, 1));
 });
